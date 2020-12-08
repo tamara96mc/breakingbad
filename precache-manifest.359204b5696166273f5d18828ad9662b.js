aa4353f50dc9b0e3f031336efffa44ab25d8d3af
tamara96mc <tamara.96mc@gmail.com>
2020-12-08 20:29:23 +0100

    Updates

---
+++ precache-manifest.359204b5696166273f5d18828ad9662b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325e05e77ed8a229e94e3397c8105b33",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "59b14bc15e6c17ea7930",
+    "url": "/breakingbad/static/css/main.4c07be54.chunk.css"
+  },
+  {
+    "revision": "506f2c560db4b98c0aa3",
+    "url": "/breakingbad/static/js/2.99bb4736.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/breakingbad/static/js/2.99bb4736.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59b14bc15e6c17ea7930",
+    "url": "/breakingbad/static/js/main.82ed3e14.chunk.js"
+  },
+  {
+    "revision": "00d7ed1ad02c6547b002",
+    "url": "/breakingbad/static/js/runtime-main.2119c628.js"
+  },
+  {
+    "revision": "6bf365bad9d7f01aa1d384019a0dca92",
+    "url": "/breakingbad/static/media/logo.6bf365ba.svg"
+  }
+]);
